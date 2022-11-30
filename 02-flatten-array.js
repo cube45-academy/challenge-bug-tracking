c847090309e2536d9ea2e9721f033299f2527907
thomas-ode <thomas.ode.w@gmail.com>
2022-11-30 16:11:33 +0100

    resolve chall 3

--- 02-flatten-array.js
+++ 02-flatten-array.js
@@ -9,18 +9,7 @@
  * @param {*} arr The array to flatten
  */
 export function flattenArray(arr) {
-    // TODO: ISSUE #4242 : Our customer reported that this method doesn't work in the case of deeply nested arrays
-    // I added the test case to the spec file, but I'm not sure how to fix it. Can you help?
-    // const result = [];
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (Array.isArray(arr[i])) {
-    //         for (let j = 0; j < arr[i].length; j++) {
-    //             result.push(arr[i][j]);
-    //         }
-    //     } else {
-    //         result.push(arr[i]);
-    //     }
-    // }
-    const result = arr.flat(Infinity);
-    return result;
+    return arr.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten);
+    }, []);
 }
